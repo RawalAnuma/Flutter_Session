3cb3c2fa226c49eb62898bf7371e8025ccaf41d2
RawalAnuma <rawalanuma@gmail.com>
2026-07-15 01:12:43 +0545

    Implementation of rows columns

--- lib/acme_home_page.dart
+++ lib/acme_home_page.dart
@@ -8,144 +8,124 @@ class AcmeHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: CustomText(
-            text: "Acme Home",
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
+        title: SafeArea(
+          child: Center(
+            child: CustomText(
+              text: "Acme Home",
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
       ),
-
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(left: 40, right: 40),
-
+          padding: const EdgeInsets.all(20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-
             children: [
-              SizedBox(height: 40),
+              SizedBox(height: 30),
               CustomText(
                 text: "Team",
                 fontSize: 25,
-                fontWeight: FontWeight.w600,
+                fontWeight: FontWeight.w500,
               ),
-
-              SizedBox(height: 10),
-              Divider(thickness: 1),
-              SizedBox(height: 10),
-
+              Divider(thickness: 2),
+              SizedBox(height: 15),
               Row(
                 children: [
                   Icon(Icons.star),
                   SizedBox(width: 10),
-                  CustomText(text: "What's New"),
+                  Text("What's New"),
                 ],
               ),
-
               SizedBox(height: 15),
-
               Row(
                 children: [
                   Icon(Icons.flag),
                   SizedBox(width: 10),
-                  CustomText(text: "Mission, Vission, Values"),
+                  Text("Mission, Vision, Values"),
                 ],
               ),
               SizedBox(height: 15),
-
               Row(
                 children: [
                   Icon(Icons.toys),
                   SizedBox(width: 10),
-                  CustomText(text: "Company Goals - 2019"),
+                  Text("Company Goals - 2019"),
                 ],
               ),
-
               SizedBox(height: 15),
-
               Row(
                 children: [
                   Icon(Icons.phone),
                   SizedBox(width: 10),
-                  CustomText(text: "Employee Directory"),
+                  Text("Employee Directory"),
                 ],
               ),
               SizedBox(height: 15),
-
               Row(
                 children: [
                   Icon(Icons.campaign),
                   SizedBox(width: 10),
-                  CustomText(text: "Recent Press"),
+                  Text("Recent Press"),
                 ],
               ),
               SizedBox(height: 15),
-
               Row(
                 children: [
                   Icon(Icons.engineering),
                   SizedBox(width: 10),
-                  CustomText(text: "Engineering Wiki"),
+                  Text("Engineering Wiki"),
                 ],
               ),
-              SizedBox(height: 40),
-
+              SizedBox(height: 50),
               CustomText(
                 text: "Policies",
                 fontSize: 25,
-                fontWeight: FontWeight.w600,
+                fontWeight: FontWeight.w500,
               ),
-
-              SizedBox(height: 10),
-              Divider(thickness: 1),
-              SizedBox(height: 10),
-
+              Divider(thickness: 2),
+              SizedBox(height: 15),
               Row(
                 children: [
                   Icon(Icons.edit_document),
                   SizedBox(width: 10),
-                  CustomText(text: "Office Manual"),
+                  Text("Office Manual"),
                 ],
               ),
               SizedBox(height: 15),
-
               Row(
                 children: [
                   Icon(Icons.train),
                   SizedBox(width: 10),
-                  CustomText(text: "Vacation"),
+                  Text("Vacation"),
                 ],
               ),
               SizedBox(height: 15),
-
               Row(
                 children: [
                   Icon(Icons.close),
                   SizedBox(width: 10),
-                  CustomText(text: "Request Time Off"),
+                  Text("Request Time Off"),
                 ],
               ),
               SizedBox(height: 15),
-
               Row(
                 children: [
                   Icon(Icons.coffee),
                   SizedBox(width: 10),
-                  CustomText(text: "Benefits Policies"),
+                  Text("Benefits Policies"),
                 ],
               ),
               SizedBox(height: 15),
-
               Row(
                 children: [
                   Icon(Icons.policy),
                   SizedBox(width: 10),
-                  CustomText(text: "Expense Policy"),
+                  Text("Expense Policy"),
                 ],
               ),
-              SizedBox(height: 15),
             ],
           ),
         ),
