6503423409372f99aad206fa8590cee1d101682a
RawalAnuma <rawalanuma@gmail.com>
2026-07-18 12:42:16 +0545

    Common content for the phone card

--- lib/common/common_phone_card.dart
+++ lib/common/common_phone_card.dart
@@ -3,32 +3,55 @@ import 'package:my_app/common/custom_text.dart';
 
 class CommonPhoneCard extends StatelessWidget {
   final String name;
-  const CommonPhoneCard({super.key, required this.name});
+  final String number;
+  final Color color;
+
+  const CommonPhoneCard({
+    super.key,
+    required this.name,
+    required this.number,
+    this.color = Colors.blue,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        border: Border.all(color: Colors.grey, width: 0.5),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(Radius.circular(50)),
+    return Row(
+      children: [
+        Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+                color: color,
+              ),
             ),
-
-            child: Icon(Icons.phone, color: Colors.white),
-          ),
-          CustomText(text: name, fontSize: 20, color: Colors.black),
-          Icon(Icons.keyboard_arrow_right, size: 32, color: Colors.blue),
-        ],
-      ),
+            Positioned(
+              left: 2.5,
+              child: Icon(Icons.person_rounded, size: 55, color: Colors.white),
+            ),
+          ],
+        ),
+        SizedBox(width: 20),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CustomText(text: name, color: Colors.black),
+            CustomText(
+              text: number,
+              color: const Color.fromARGB(255, 93, 92, 92),
+            ),
+          ],
+        ),
+        Spacer(),
+        Icon(
+          Icons.keyboard_arrow_right,
+          size: 40,
+          color: const Color.fromARGB(255, 93, 92, 92),
+        ),
+      ],
     );
   }
 }
