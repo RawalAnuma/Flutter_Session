3efd20a4d88bba0d37d592d16da3839d8cd4649e
RawalAnuma <rawalanuma@gmail.com>
2026-07-18 12:46:23 +0545

    called respective dart page in main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/acme_home_page.dart';
 //import 'package:my_app/acme_home_page.dart';
 //import 'package:my_app/contact_page.dart';
 import 'package:my_app/contact_list_page.dart';
@@ -15,6 +16,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // return MaterialApp(title: 'Profile page', home: const ProfilePage());
-    return MaterialApp(title: "Acme Home Page", home: const ContactListPage());
+    //return MaterialApp(title: "Acme Home Page", home: const AcmeHomePage());
+    return MaterialApp(
+      title: "Contct List Page",
+      home: const ContactListPage(),
+    );
   }
 }
