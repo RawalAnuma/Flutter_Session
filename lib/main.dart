ae752ca467a0a68d616b13572451b388ec5505c0
RawalAnuma <rawalanuma@gmail.com>
2026-07-22 17:26:55 +0545

    added proper route

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/acme_home_page.dart';
+import 'package:my_app/brand_page.dart';
 //import 'package:my_app/acme_home_page.dart';
 //import 'package:my_app/contact_page.dart';
 import 'package:my_app/contact_list_page.dart';
+import 'package:my_app/expanded_demo.dart';
+import 'package:my_app/login_page.dart';
+import 'package:my_app/order_page.dart';
+import 'package:my_app/product_page.dart';
 // import 'package:my_app/profile_page.dart';
 
 void main() {
@@ -17,9 +22,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // return MaterialApp(title: 'Profile page', home: const ProfilePage());
     //return MaterialApp(title: "Acme Home Page", home: const AcmeHomePage());
-    return MaterialApp(
-      title: "Contct List Page",
-      home: const ContactListPage(),
-    );
+    //return MaterialApp(title: "Contct List Page", home: const ContactListPage(),);
+    //return MaterialApp(title: "Brand Page", home: const BrandPage());
+    //return MaterialApp(title: "Order Page", home: const OrderPage());
+    //return MaterialApp(title: "Product Page", home: const ProductPage());
+    return MaterialApp(title: "Login Page", home: LoginPage());
   }
 }
