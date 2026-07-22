51d7058dca241b438fcaf1cac7426cc33e591e79
RawalAnuma <rawalanuma@gmail.com>
2026-07-22 17:19:33 +0545

    Path for the image is stored

---
+++ lib/core/app_image.dart
@@ -0,0 +1,12 @@
+class AppImage {
+  //path
+  static const String path = "assets/images";
+
+  //images
+  static const String shoe1 = "$path/shoe1.jpeg";
+  static const String shoe2 = "$path/shoe2.jpeg";
+  static const String shoe3 = "$path/shoe3.jpeg";
+  static const String shoe4 = "$path/shoe4.jpeg";
+  static const String shoe5 = "$path/shoe5.jpeg";
+  static const String shoe6 = "$path/shoe6.jpeg";
+}
