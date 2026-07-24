0d066e2f4f726925e168f187a53dcd72a2ca6f1e
RawalAnuma <rawalanuma@gmail.com>
2026-07-24 18:20:24 +0545

    ChangeNotifierProvider added

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/acme_home_page.dart';
-import 'package:my_app/brand_page.dart';
+import 'package:my_app/screens/acme_home_page.dart';
+import 'package:my_app/screens/brand_page.dart';
 //import 'package:my_app/acme_home_page.dart';
 //import 'package:my_app/contact_page.dart';
-import 'package:my_app/contact_list_page.dart';
-import 'package:my_app/expanded_demo.dart';
-import 'package:my_app/login_page.dart';
-import 'package:my_app/order_page.dart';
-import 'package:my_app/product_page.dart';
+import 'package:my_app/screens/contact_list_page.dart';
+import 'package:my_app/screens/expanded_demo.dart';
+import 'package:my_app/screens/login_page.dart';
+import 'package:my_app/screens/order_page.dart';
+import 'package:my_app/screens/product_page.dart';
 import 'package:my_app/provider/counter_provider.dart';
+import 'package:my_app/provider/todo_provider.dart';
 import 'package:provider/provider.dart';
 // import 'package:my_app/profile_page.dart';
 
 void main() {
   runApp(
     ChangeNotifierProvider(
-      create: (_) => CounterProvider(),
+      create: (_) => TodoProvider(),
       child: const MyApp(),
     ),
   );
@@ -31,9 +32,9 @@ class MyApp extends StatelessWidget {
     //return MaterialApp(title: "Acme Home Page", home: const AcmeHomePage());
     //return MaterialApp(title: "Contct List Page", home: const ContactListPage(),);
     //return MaterialApp(title: "Brand Page", home: const BrandPage());
-    return MaterialApp(title: "Brand Page", home: const ExpandedDemo());
+    //return MaterialApp(title: "Brand Page", home: const ExpandedDemo());
     //return MaterialApp(title: "Order Page", home: const OrderPage());
     //return MaterialApp(title: "Product Page", home: const ProductPage());
-    //return MaterialApp(title: "Login Page", home: LoginPage());
+    return MaterialApp(title: "Login Page", home: LoginPage());
   }
 }
