5dc628fe49203bd986b6cf1b3c97ea8b3c535712
RawalAnuma <rawalanuma@gmail.com>
2026-07-24 21:56:40 +0545

    return statement for SimpleTodoPage

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,13 @@ import 'package:my_app/screens/order_page.dart';
 import 'package:my_app/screens/product_page.dart';
 import 'package:my_app/provider/counter_provider.dart';
 import 'package:my_app/provider/todo_provider.dart';
+import 'package:my_app/screens/simple_todo_page.dart';
 import 'package:provider/provider.dart';
 // import 'package:my_app/profile_page.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => TodoProvider(),
-      child: const MyApp(),
-    ),
+    ChangeNotifierProvider(create: (_) => TodoProvider(), child: const MyApp()),
   );
 }
 
@@ -32,9 +30,10 @@ class MyApp extends StatelessWidget {
     //return MaterialApp(title: "Acme Home Page", home: const AcmeHomePage());
     //return MaterialApp(title: "Contct List Page", home: const ContactListPage(),);
     //return MaterialApp(title: "Brand Page", home: const BrandPage());
-    //return MaterialApp(title: "Brand Page", home: const ExpandedDemo());
+    //return MaterialApp(title: "Expanded Page", home: const ExpandedDemo());
     //return MaterialApp(title: "Order Page", home: const OrderPage());
     //return MaterialApp(title: "Product Page", home: const ProductPage());
-    return MaterialApp(title: "Login Page", home: LoginPage());
+    //return MaterialApp(title: "Login Page", home: LoginPage());
+    return MaterialApp(title: "Todo Page", home: SimpleTodoPage());
   }
 }
